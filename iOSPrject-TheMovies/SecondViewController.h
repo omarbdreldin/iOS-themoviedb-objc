8ab494f6f4e9bc5802a316982479c3f65034e8d6
Omar Bdreldin <omarbdreldin@learniosfreecoms-mac.local>
2018-05-06 13:58:26 +0200

    Initial Commit

---
+++ iOSPrject-TheMovies/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  iOSPrject-TheMovies
+//
+//  Created by Omar Bdreldin on 5/6/18.
+//  Copyright © 2018 Omar Bdreldin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ iOSPrject-TheMovies/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSPrject-TheMovies
+//
+//  Created by Omar Bdreldin on 5/6/18.
+//  Copyright © 2018 Omar Bdreldin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
